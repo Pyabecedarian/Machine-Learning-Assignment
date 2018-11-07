c03f388fc54d4b7222159c45cc5db72ede75f194
Pyabecedarian <liu-quan@hotmail.com>
2018-11-07 19:51:14 +0800

    'ex1'

---
+++ ml_ex1/ex1.py
@@ -0,0 +1,8 @@
+# fprintf('Running warmUpExercise ... \n');
+# fprintf('5x5 Identity Matrix: \n');
+
+print('Running warmUpExercise ... \n')
+print('5x5 Identity Matrix: \n')
+
+from ml_ex1.warmUpExercise import warmUpExercise
+warmUpExercise()
---
+++ ml_ex1/warmUpExercise.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def warmUpExercise():
+    """
+    Create a 5x5 Identity Matrix
+    """
+    I = np.eye(5, dtype=int)
+    print('ans = \n', I)
